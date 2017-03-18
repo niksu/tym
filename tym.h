d4ee43aacc6d83df4eedf1131827415d079499aa
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2017-03-18 02:27:47 +0000

    added buffer-flushing before printing error messages;

--- tym.h
+++ tym.h
@@ -16,7 +16,7 @@
 #define BUF_SIZE 300
 
 #define VERBOSE printf
-#define ERR(...) fprintf(stderr, __VA_ARGS__)
+#define ERR(...) fflush(stdout); fprintf(stderr, __VA_ARGS__)
 
 #if DEBUG
 #define DBG printf
