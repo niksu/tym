d68274cb7ab688dd97c0ad5f169672d200b57587
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2017-03-19 02:21:39 +0000

    split into .c and .h files;

--- symbols.c
+++ symbols.c
@@ -7,22 +7,7 @@
  * License: LGPL version 3 (for licensing terms see the file called LICENSE)
  */
 
-#include <assert.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#include "ast.h"
-
-#define HASH_RANGE 256
-// NOTE value of TERM_DATABASE_SIZE must be >= the range of the hash function for terms.
-#define TERM_DATABASE_SIZE HASH_RANGE
-
-struct term_database_t {
-  struct terms_t * herbrand_universe;
-  struct terms_t * term_database[TERM_DATABASE_SIZE];
-};
+#include "symbols.h"
 
 struct term_database_t *
 mk_term_database(void)
@@ -87,11 +72,6 @@ term_database_str(struct term_database_t * tdb, size_t * outbuf_size, char * out
   return l;
 }
 
-struct predicate_t {
-  const char * predicate;
-  uint8_t arity;
-};
-
 struct predicate_t *
 mk_pred(const char * predicate, uint8_t arity)
 {
@@ -105,11 +85,6 @@ mk_pred(const char * predicate, uint8_t arity)
   return p;
 }
 
-struct predicates_t {
-  struct predicate_t * predicate;
-  struct predicates_t * next;
-};
-
 struct predicates_t *
 mk_pred_cell(struct predicate_t * pred, struct predicates_t * next)
 {
@@ -122,8 +97,6 @@ mk_pred_cell(struct predicate_t * pred, struct predicates_t * next)
   ps->next = next;
   return ps;
 }
-
-typedef enum {NO_ERROR_eq_pred, SAME_PREDICATE_DIFF_ARITY} eq_pred_error_t;
 
 bool
 eq_pred(struct predicate_t p1, struct predicate_t p2, eq_pred_error_t * error_code, bool * result)
@@ -149,13 +122,6 @@ eq_pred(struct predicate_t p1, struct predicate_t p2, eq_pred_error_t * error_co
   }
 }
 
-#define ATOM_DATABASE_SIZE HASH_RANGE
-
-struct atom_database_t {
-  struct term_database_t * tdb;
-  struct predicates_t * atom_database[ATOM_DATABASE_SIZE];
-};
-
 struct atom_database_t *
 mk_atom_database(void)
 {
@@ -166,8 +132,6 @@ mk_atom_database(void)
   }
   return result;
 }
-
-typedef enum {ADL_NO_ERROR, DIFF_ARITY} adl_lookup_error_t;
 
 bool
 atom_database_member(struct atom_t * atom, struct atom_database_t * adb, adl_lookup_error_t error_code, bool * result)
@@ -218,8 +182,6 @@ atom_database_member(struct atom_t * atom, struct atom_database_t * adb, adl_loo
 
   return success;
 }
-
-typedef enum {NO_ATOM_DATABASE} adl_add_error_t;
 
 bool
 atom_database_add(struct atom_t * atom, struct atom_database_t * adb, adl_add_error_t * error_code, struct predicate_t ** result)
