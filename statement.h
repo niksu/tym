e92aab492ef8ea89506e8b92325f2dce4fb4bbb3
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2017-04-05 02:18:05 +0100

    refactored the statement-related API;

--- statement.h
+++ statement.h
@@ -11,13 +11,16 @@
 #define __TYM_STATEMENT_H__
 
 // NOTE only interested in finite models
-typedef const struct terms_t * const contents universe_t;
+struct universe_t {
+  uint8_t cardinality;
+  char ** element;
+};
 
 struct stmt_const_t {
   const char * const_name;
   struct terms_t * params;
   struct fmla_t * body;
-}
+};
 
 enum stmt_kind_t {STMT_AXIOM, STMT_CONST_DEF};
 
@@ -29,14 +32,14 @@ struct stmt_t {
   } param;
 };
 
-universe_t * mk_universe(struct terms_t *);
+struct universe_t * mk_universe(struct terms_t *);
+size_t universe_str(struct universe_t *, size_t * remaining, char * buf);
+void free_universe(struct universe_t *);
+
 struct stmt_t * mk_stmt_axiom(struct fmla_t * axiom);
 struct stmt_t * mk_stmt_pred(char * pred_name, struct terms_t * params, struct fmla_t * body);
-struct stmt_t * mk_stmt_const(char * const_name, universe_t *);
-
-size_t universe_str(universe_t *, size_t * remaining, char * buf);
+struct stmt_t * mk_stmt_const(char * const_name, struct universe_t *);
 size_t stmt_str(struct stmt_t *, size_t * remaining, char * buf);
-
 void free_stmt(struct stmt_t *);
 
 struct stmts_t {
@@ -49,13 +52,13 @@ size_t stmts_str(struct stmts_t *, size_t * remaining, char * buf);
 void free_stmts(struct stmts_t *);
 
 struct model_t {
-  universe_t universe;
+  struct universe_t universe;
   struct stmts_t stmts;
-}
+};
 
-struct model_t * mk_model(universe_t *);
-void strengthen_model(model_t *, stmt_t *);
+struct model_t * mk_model(struct universe_t *);
 size_t model_str(struct model_t *, size_t * remaining, char * buf);
 void free_model(struct model_t *);
+void strengthen_model(struct model_t *, struct stmt_t *);
 
 #endif /* __TYM_STATEMENT_H__ */
