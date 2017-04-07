cb86483da5f360999219fa42f39107cb6dc22b4c
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2017-04-07 02:16:02 +0100

    fixed offset bugs (i.e., 'buf + l' rather than 'buf'); fixed termination bug: added null character at end of string; added copying of const names, to avoid attempting to later free temporary auto-assigned strings; added test_statement -- this needs more work to test more comprehensively;

--- statement.c
+++ statement.c
@@ -15,6 +15,7 @@
 #include "ast.h"
 #include "formula.h"
 #include "statement.h"
+#include "tym.h"
 
 struct universe_t *
 mk_universe(struct terms_t * terms)
@@ -126,7 +127,9 @@ mk_stmt_const(char * const_name, struct universe_t * uni)
   struct stmt_t * result = malloc(sizeof(struct stmt_t));
   struct stmt_const_t * sub_result = malloc(sizeof(struct stmt_const_t));
 
-  sub_result->const_name =const_name;
+  char * const_name_copy = malloc(sizeof(char) * strlen(const_name));
+  strcpy(const_name_copy, const_name);
+  sub_result->const_name = const_name_copy;
   sub_result->params = NULL;
 
   struct fmlas_t * fmlas = NULL;
@@ -173,7 +176,7 @@ stmt_str(struct stmt_t * stmt, size_t * remaining, char * buf)
       *remaining -= strlen(&(buf[l]));
       l += strlen(&(buf[l]));
 
-      l += fmla_str(stmt->param.const_def->body, remaining, buf);
+      l += fmla_str(stmt->param.const_def->body, remaining, buf + l);
 
       buf[(*remaining)--, l++] = ')';
       buf[(*remaining)--, l++] = ')';
@@ -187,7 +190,7 @@ stmt_str(struct stmt_t * stmt, size_t * remaining, char * buf)
       while (NULL != params_cursor) {
         buf[(*remaining)--, l++] = '(';
 
-        l += term_to_str(params_cursor->term, remaining, buf);
+        l += term_to_str(params_cursor->term, remaining, buf + l);
 
         sprintf(&(buf[l]), " Universe)");
         *remaining -= strlen(&(buf[l]));
@@ -226,7 +229,9 @@ free_stmt(struct stmt_t * stmt)
     break;
   case STMT_CONST_DEF:
     free((char *)stmt->param.const_def->const_name);
-    free_terms(stmt->param.const_def->params);
+    if (NULL != stmt->param.const_def->params) {
+      free_terms(stmt->param.const_def->params);
+    }
     free_fmla(stmt->param.const_def->body);
     free(stmt->param.const_def);
     break;
@@ -268,7 +273,9 @@ free_stmts(struct stmts_t * stmts)
 {
   assert(NULL != stmts->stmt);
   free_stmt(stmts->stmt);
-  free_stmts(stmts->next);
+  if (NULL != stmts->next) {
+    free_stmts(stmts->next);
+  }
   free(stmts);
 }
 
@@ -286,7 +293,8 @@ model_str(struct model_t * m, size_t * remaining, char * buf)
 {
   size_t l = 0;
   l += universe_str(m->universe, remaining, buf);
-  l += stmts_str(m->stmts, remaining, buf);
+  l += stmts_str(m->stmts, remaining, buf + l);
+  buf[l] = '\0';
   return l;
 }
 
@@ -302,4 +310,31 @@ void
 strengthen_model(struct model_t * m, struct stmt_t * stmt)
 {
   m->stmts = mk_stmt_cell(stmt, m->stmts);
+}
+
+void
+test_statement()
+{
+  struct term_t * aT = mk_term(CONST, "a");
+  struct term_t * bT = mk_term(CONST, "b");
+  struct terms_t * terms = mk_term_cell(aT, NULL);
+  terms = mk_term_cell(bT, terms);
+
+  struct model_t * m = mk_model(mk_universe(terms));
+
+//  struct stmt_t * s1S = mk_stmt_axiom(struct fmla_t * axiom);
+//  struct stmt_t * s2S = mk_stmt_pred(char * pred_name, struct terms_t * params, struct fmla_t * body);
+  struct stmt_t * s3S = mk_stmt_const("x", m->universe);
+
+//  strengthen_model(m, s1S);
+//  strengthen_model(m, s2S);
+  strengthen_model(m, s3S);
+
+  size_t remaining_buf_size = BUF_SIZE;
+  char * buf = malloc(remaining_buf_size);
+  size_t l = model_str(m, &remaining_buf_size, buf);
+  printf("test model (size=%zu, remaining=%zu)\n|%s|\n", l, remaining_buf_size, buf);
+
+  free_model(m);
+  free(buf);
 }
