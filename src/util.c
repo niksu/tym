13d60ef11459115032de3a0d11cc1951344595fd
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2017-12-24 08:35:40 -0500

    fixed bug: wasn't properly terminating prefixed strings;

--- src/util.c
+++ src/util.c
@@ -18,5 +18,6 @@ strcpy_prefixed(const char * prefix, const char * str)
   char * result = malloc(strlen(prefix) + strlen(str) + 1);
   memcpy(result, prefix, strlen(prefix));
   memcpy(result + strlen(prefix), str, strlen(str));
+  result[strlen(prefix) + strlen(str)] = '\0';
   return result;
 }
