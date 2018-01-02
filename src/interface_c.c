bf71416ea55769178b45548e9c35416d716c79e1
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2018-01-02 09:09:08 -0500

    tuned;

--- src/interface_c.c
+++ src/interface_c.c
@@ -21,13 +21,12 @@ tym_csyntax_term(struct TymSymGen * namegen, const struct TymTerm * term)
   struct TymCSyntax * result = malloc(sizeof(*result));
 
   const char * identifier = tym_decode_str(term->identifier);
-  const char * template = "(struct TymTerm){.kind = ,\
-    .identifier = TYM_CSTR_DUPLICATE(\"\")}";
+  const char * template = "(struct TymTerm){.kind = , .identifier = TYM_CSTR_DUPLICATE(\"\")}";
   char * str_buf = malloc(sizeof(*str_buf) *
       (strlen(identifier) + strlen(TymTermKindStr[term->kind]) +
        strlen(template) + 1));
   int buf_occupied = sprintf(str_buf, "(struct TymTerm){.kind = %s, .identifier = TYM_CSTR_DUPLICATE(\"%s\")}",
-    TymTermKindStr[term->kind], tym_decode_str(term->identifier));
+    TymTermKindStr[term->kind], identifier);
   assert(buf_occupied > 0);
   assert(strlen(str_buf) == (unsigned long)buf_occupied);
 
