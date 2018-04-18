e99411feca17907cd7acb861233be95fd677b7dd
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2018-04-18 00:29:14 -0400

    continued work from 28ae0253231ba22ef43cf293a2936063f9bec965 to work on TymClause pointer;

--- src/interface_c.c
+++ src/interface_c.c
@@ -265,11 +265,11 @@ const struct TymCSyntax * tym_csyntax_program(struct TymSymGen * namegen, const 
 
     str_buf_args = tym_decode_str(tym_append_str_destructive2(sub_csyns[i]->serialised, tym_encode_str(str_buf_args)));
 
-    array[i] = sub_csyns[i]->name;
+    array[i] = tym_csyntax_address_of(sub_csyns[i]);
   }
 
   const TymStr * args_identifier = NULL;
-  const TymStr * array_type = TYM_CSTR_DUPLICATE("struct TymClause");
+  const TymStr * array_type = TYM_CSTR_DUPLICATE("struct TymClause *");
   const TymStr * array_str = tym_array_of(namegen, &args_identifier, prog->no_clauses, array_type, array);
 
   str_buf_args = tym_decode_str(tym_append_str_destructive1(tym_encode_str(str_buf_args), array_str));
